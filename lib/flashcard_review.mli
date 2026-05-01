69b2a6d67e688cb435357728f8e27d3ec08be044
smriti-kumar <mailsmritikumar@gmail.com>
2026-05-01 00:02:38 -0400

    added methods and types to flashcard review mli that were not exposed

--- lib/flashcard_review.mli
+++ lib/flashcard_review.mli
@@ -1,6 +1,6 @@
-type confidence
-type flashcard_record
-type review_stats
+type confidence = Low | Medium | High
+type flashcard_record = (string * string) * confidence * int
+type review_stats = (string * string) * bool * bool * confidence
 
 val string_to_words : string -> string list
 val wrap_string : string -> int -> string list
@@ -9,6 +9,7 @@ val optimal_order : review_stats list -> (string * string) list
 val stat_to_csv : int -> review_stats -> string
 val csv_to_stat : string -> review_stats
 val get_session : string -> int
+val read_last_row : string -> int
 val stats_to_csv : review_stats list -> int -> string
 val load_last : string -> review_stats list
 val load_all : string -> (int * review_stats) list
